95b5d8d92b9a3c1c4a62e893660990bd7095c177
felipesalesJ <31224106+felipesalesJ@users.noreply.github.com>
2017-10-18 16:35:19 -0200

    Add files via upload

--- atividadevi-votacao/src/main/java/br/com/atividadevi/Beans/Beans.java
+++ atividadevi-votacao/src/main/java/br/com/atividadevi/Beans/Beans.java
@@ -1,5 +1,19 @@
 package br.com.atividadevi.Beans;
 
-public class Beans {
+import java.io.Serializable;
 
+import javax.faces.context.FacesContext;
+import javax.faces.application.FacesMessage;
+
+public class Beans implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	public void addMensageError(String mensage){
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, mensage, mensage));
+	}
+	
+	public void addMensageInfo(String mensage){
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, mensage, mensage));
+	}
 }
